88d14e84274e9b1b96aa1d107d42dc3cd039e748
Sereni <sereni.nm@gmail.com>
2015-10-11 11:20:48 +0300

    Initial commit -- add AFNetworking

---
+++ weather-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  weather-Bridging-Header.h
+//  weather
+//
+//  Created by Sereni Rikheart on 10/11/15.
+//  Copyright © 2015 Sereni Rikheart. All rights reserved.
+//
+
+#ifndef weather_Bridging_Header_h
+#define weather_Bridging_Header_h
+
+
+#endif /* weather_Bridging_Header_h */
+#import "AFNetworking.h"
--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // connect table to code
+    @IBOutlet weak var weatherTableView: UITableView!
+    
+    // holds table data
+    var tableData = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // todo get city id from somewhere
+        let cityID = "524901"
+        getWeatherFor(cityID)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func getWeatherFor(cityID: String) {
+        // Call OpenWeather API with a given city id and pre-defined api key
+        let APIKey = "d15b15acb4da9baaaaacd99f93427014"
+        
+        // Construct url
+        let URL = "http://api.openweathermap.org/data/2.5/forecast/city?id=\(cityID)&APPID=\(APIKey)"
+        
+        let manager: AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
+        
+        manager.GET(URL,
+            parameters: nil,
+            success: {(operation: AFHTTPRequestOperation, responseObject: AnyObject) -> Void in
+                print("\(responseObject)")
+            },
+            failure: {(operation: AFHTTPRequestOperation, error: NSError) -> Void in
+                print("\(error)")})
+
+    }
 
 }
-
